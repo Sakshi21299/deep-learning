7d54e78a0ef5dfeb1cc4a14ed0af623422422c4a
Sakshi <73687517+Sakshi21299@users.noreply.github.com>
2024-03-17 12:48:31 -0400

    Create setup.py
    Setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mof_networks",
+    description=(
+        "Deep learning approaches for predciting MOF parameters"
+    ),
+    author="Sakshi Naik and Yu-Shan Chang",
+    packages=["mof_net"],
+)
